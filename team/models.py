27a29b46b91d42b1f0b42aa642f46a557d640607
OldGreenIsMe <1176606531@qq.com>
2022-08-01 15:13:10 +0800

    team/models.py complete

--- team/models.py
+++ team/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Team(models.Model):
+    teamid = models.AutoField(primary_key=True)
+    teamname = models.CharField(max_length=100, default='')
+
+
+class User(models.Model):
+    userid = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100, default='')
+    truename = models.CharField(max_length=100, default='')
+    password = models.CharField(max_length=100, default='')
+    photo = models.ImageField(upload_to='img', default='img/default_photo.png')
+    team_belonged = models.ManyToManyField(Team, through='UserTeam')
+    email = models.EmailField()
+
+
+
+class UserTeam(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    permission = models.IntegerField(default=0)  # 0为普通成员，1为管理员, 2为创始者
