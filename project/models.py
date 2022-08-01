29eaeeecae554efaf0e2e0236d98c75f1bcf5dc5
SHLPRN <20373672@buaa.edu.cn>
2022-08-01 15:54:24 +0800

    merge changes in models.py

--- project/models.py
+++ project/models.py
@@ -6,11 +6,11 @@ from team.models import User
 class Project(models.Model):
     projId = models.AutoField(primary_key=True)
     projName = models.CharField(max_length=50, unique=True)
-    projCreator = models.ForeignKey(to=User, null=False, blank=False)
+    projCreator = models.ForeignKey(to=User, null=False, blank=False, on_delete=models.SET_NULL)
     # 用于判断是否被删除到回收站中 1-未被删除 2-回收站中
     status = models.IntegerField(default=1)
     # 记录删除的用户（ID）及删除时间
-    deletePerson = models.ForeignKey(to=User, null=True, blank=True)
+    deletePerson = models.ForeignKey(to=User, null=True, blank=True, on_delete=models.SET_NULL)
     deleteTime = models.CharField(max_length=20)
 
 
@@ -18,12 +18,11 @@ class Project(models.Model):
 class File(models.Model):
     fileId = models.AutoField(primary_key=True)
     fileName = models.CharField(max_length=50, unique=True)
-    fileCreator = models.ForeignKey(to=User, null=False, blank=False)
-    projectId = models.ForeignKey(to=Project, null=False, blank=False)
+    fileCreator = models.ForeignKey(to=User, null=False, blank=False, on_delete=models.SET_NULL)
+    projectId = models.ForeignKey(to=Project, null=False, blank=False, on_delete=models.SET_NULL)
 
 
 # 原型类（具体存什么东西还没定，等前端确定）
 class Prototype(models.Model):
     prototypeId = models.AutoField(primary_key=True)
-    projectId = models.ForeignKey(to=Project, null=False, blank=False)
-
+    projectId = models.ForeignKey(to=Project, null=False, blank=False, on_delete=models.SET_NULL)
