a2517daa0078baed01a7a391e6e93087d6d335ff
SHLPRN <20373672@buaa.edu.cn>
2022-08-08 14:41:26 +0800

    修改文档中心函数

--- project/models.py
+++ project/models.py
@@ -1,8 +1,8 @@
-from django.db import models
-from team.models import User, Team
 from utils import storage
+from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import pre_delete
+from team.models import User, Team
 
 
 # 项目类
