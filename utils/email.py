821476d98204176822e8200135b1eedd6756d67c
SHLPRN <20373672@buaa.edu.cn>
2022-08-09 16:18:15 +0800

    完善解散团队发送邮件通知的功能

--- utils/email.py
+++ utils/email.py
@@ -21,12 +21,12 @@ def sendVerifyCodeMethod(toEmail, mode):
         data = input1 + verifyCodeNum[0] + input3 + verifyCodeNum[1] + input3 + verifyCodeNum[2] + input3 + \
                verifyCodeNum[3] + input3 + verifyCodeNum[4] + input3 + verifyCodeNum[5] + input2
         message = MIMEText(data, 'html', 'utf-8')
-        message['Subject'] = Header('墨书账号注册验证码')     # 邮件主题
+        message['Subject'] = Header('墨书-账号注册验证码')     # 邮件主题
     else:
         data = input4 + verifyCodeNum[0] + input3 + verifyCodeNum[1] + input3 + verifyCodeNum[2] + input3 + \
                verifyCodeNum[3] + input3 + verifyCodeNum[4] + input3 + verifyCodeNum[5] + input2
         message = MIMEText(data, 'html', 'utf-8')
-        message['Subject'] = Header('墨书账号修改密码验证码')
+        message['Subject'] = Header('墨书-账号修改密码验证码')
     message['From'] = Header('墨书团队')                    # 邮件发送者
     message['To'] = Header(toEmail)
     mail = smtplib.SMTP()
@@ -55,7 +55,7 @@ def inviteMemberSendMethod(invitorName, userName, userId, teamName, teamId, toEm
         input5 = f.read()
     msg = input1 + userName + input2 + invitorName + input3 + teamName + input4 + dstUrl + input5
     message = MIMEText(msg, 'html', 'utf-8')
-    message['Subject'] = Header('墨书团队邀请邮件')
+    message['Subject'] = Header('墨书-团队邀请邮件')
     message['From'] = Header('墨书团队')
     message['To'] = Header(toEmail)
     mail = smtplib.SMTP()
@@ -89,7 +89,7 @@ def applyJoinMethod(adminName, userName, userId, teamName, teamId, toEmail):
         input5 = f.read()
     msg = input1 + adminName + input2 + userName + input3 + teamName + input4 + dstUrl + input5
     message = MIMEText(msg, 'html', 'utf-8')
-    message['Subject'] = Header('墨书申请加入团队邮件')
+    message['Subject'] = Header('墨书-申请加入团队邮件')
     message['From'] = Header('墨书团队')
     message['To'] = Header(toEmail)
     mail = smtplib.SMTP()
@@ -109,7 +109,27 @@ def deleteNotice(adminName, userName, teamName, toEmail):
         input4 = f.read()
     data = input1 + userName + input2 + adminName + input3 + teamName + input4
     message = MIMEText(data, 'html', 'utf-8')
-    message['Subject'] = Header('墨书通知邮件')
+    message['Subject'] = Header('墨书-移出团队通知邮件')
+    message['From'] = Header('墨书团队')        # 邮件发送者
+    message['To'] = Header(toEmail)
+    mail = smtplib.SMTP()
+    mail.connect("smtp.qq.com")
+    mail.login("805659773@qq.com", settings.SECRETS['email_key'])
+    mail.sendmail("805659773@qq.com", [toEmail], message.as_string())
+
+
+def deleteTeamNotice(adminName, userName, teamName, toEmail):
+    with open("./utils/deleteTeam/deleteTeam1.html", 'r', encoding='utf-8') as f:
+        input1 = f.read()
+    with open("./utils/deleteTeam/deleteTeam2.html", 'r', encoding='utf-8') as f:
+        input2 = f.read()
+    with open("./utils/deleteTeam/deleteTeam3.html", 'r', encoding='utf-8') as f:
+        input3 = f.read()
+    with open("./utils/deleteTeam/deleteTeam4.html", 'r', encoding='utf-8') as f:
+        input4 = f.read()
+    data = input1 + userName + input2 + adminName + input3 + teamName + input4
+    message = MIMEText(data, 'html', 'utf-8')
+    message['Subject'] = Header('墨书-团队解散通知邮件')
     message['From'] = Header('墨书团队')        # 邮件发送者
     message['To'] = Header(toEmail)
     mail = smtplib.SMTP()
